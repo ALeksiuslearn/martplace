4b1574f8fe3c292d0313906c0e2631c9cfd02e5f
ALeksiuslearn <igotgoldenroshan@gmail.com>
2020-07-08 20:26:46 +0300

    slider semidone

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,9 @@
-$(function () {});
+$(function () {
+  $(".weekly__items").slick({
+    nextArrow:
+      '<button class="slick-arrow slick-next"><img src="images/weekly/icons/chevron-right.png"></button>',
+    prevArrow:
+      '<button class="slick-arrow slick-prev"><img src="../images/weekly/icons/chevron-left.png" alt="prev arrow"></button>',
+    dots: false,
+  });
+});

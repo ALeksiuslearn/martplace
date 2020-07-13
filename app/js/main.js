8bec15daf7269367692d0aa328947919bd10fc48
ALeksiuslearn <igotgoldenroshan@gmail.com>
2020-07-14 02:24:36 +0300

    added alot

--- app/js/main.js
+++ app/js/main.js
@@ -6,4 +6,22 @@ $(function () {
       '<button class="slick-arrow slick-prev"><img src="../images/weekly/icons/chevron-left.png" alt="prev arrow"></button>',
     dots: false,
   });
+  $(".followers-feed__items").slick({
+    nextArrow:
+      '<button class="slick-arrow slick-next"><img src="images/weekly/icons/chevron-right.png"></button>',
+    prevArrow:
+      '<button class="slick-arrow slick-prev"><img src="../images/weekly/icons/chevron-left.png" alt="prev arrow"></button>',
+    dots: false,
+    slidesToShow: 3,
+  });
+  $(function () {
+    $(".panel-rating").rateYo({
+      starWidth: "15px",
+      rating: 4.5,
+      readOnly: true,
+      normalFill: "#ffc000",
+      spacing: "3px",
+    });
+  });
+  var mixer = mixitup(".product__items");
 });

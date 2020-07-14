3b316a0e5111eef619b0b1b349d1c5a2d7bf44fd
ALeksiuslearn <igotgoldenroshan@gmail.com>
2020-07-14 17:15:25 +0300

    full 1rst page

--- app/js/main.js
+++ app/js/main.js
@@ -3,7 +3,7 @@ $(function () {
     nextArrow:
       '<button class="slick-arrow slick-next"><img src="images/weekly/icons/chevron-right.png"></button>',
     prevArrow:
-      '<button class="slick-arrow slick-prev"><img src="../images/weekly/icons/chevron-left.png" alt="prev arrow"></button>',
+      '<button class="slick-arrow slick-prev lnr-chevron-left"><img src="../images/weekly/icons/chevron-left.png" alt="prev arrow"></button>',
     dots: false,
   });
   $(".followers-feed__items").slick({
@@ -13,6 +13,14 @@ $(function () {
       '<button class="slick-arrow slick-prev"><img src="../images/weekly/icons/chevron-left.png" alt="prev arrow"></button>',
     dots: false,
     slidesToShow: 3,
+  });
+  $(".clients-feedback__items").slick({
+    nextArrow: '<button class="slick-arrow chevr-right"></button>',
+    prevArrow: '<button class="slick-arrow chevr-left"></button>',
+    dots: false,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    autoplay: true,
   });
   $(function () {
     $(".panel-rating").rateYo({
